fd096de2fda5fa394b64cb0ff8414988658bd218
Al Siddeg Omer <alsiddeg.omer19990@gmail.com>
2024-06-24 10:59:35 +0300

    delete feature added for products

--- pages/api/products.js
+++ pages/api/products.js
@@ -47,4 +47,12 @@ export default async function handle(req, res) {
        await Product.updateOne({_id}, {title:title, description:description, price:price});
         res.json(true)
     }
+
+
+    if (method === 'DELETE') {
+        if(req.query?.id) {
+            await Product.deleteOne({_id:req.query?.id});
+            res.json(true);
+        }
+    }
 }
